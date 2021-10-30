30ee03ee55c01e65fda59edce0930af217c22419
Nyeogmi <economicsbat@gmail.com>
2021-10-29 22:23:32 -0700

    Enhance TODOs

--- src/todo.rs
+++ src/todo.rs
@@ -5,6 +5,9 @@
 // - Arguments for Call instruction.
 // - Return values for Ret instruction.
 // - Procedure ids for Instruction set (instead of just using Rc<Procedure> everywhere)
+// - Type ids for Instruction set
+// - Separate typechecking pass
+// - Instruction to ref an individual field
 // - Instructions to make assertions about the stack.
 // - Stack values should be ref-like.
 //   Effectively: things in the stack (as opposed to variable slots) are &Xs, variable slots are non-fixed-size. Whoa!! 
