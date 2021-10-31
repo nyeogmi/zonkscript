98c8d9f0daca75136e1027d4e5e8e3c49212891e
Nyeogmi <economicsbat@gmail.com>
2021-10-30 20:31:11 -0700

    Update TODOs

--- src/todo.rs
+++ src/todo.rs
@@ -1,7 +1,8 @@
 // TODO: Things below.
-// - Allocate whole stack inline.
+// - Don't use Moogle (Use something ad hoc instead.)
+// - Get rid of hacks for 1-indexing.
+// - std_primitives.rs and linker.rs are _real ugly
 // - Variant should be Copy.
-// - Composite data types.
 // - Arguments for Call instruction.
 // - Return values for Ret instruction.
 // - Procedure ids for Instruction set (instead of just using Rc<Procedure> everywhere)
@@ -10,8 +11,5 @@
 // - Instruction to ref an individual field
 // - Instructions to make assertions about the stack.
 // - Primitives on the stack should have an "allocated?" flag
-// - Stack values should be ref-like.
-//   Effectively: things in the stack (as opposed to variable slots) are &Xs, variable slots are non-fixed-size. Whoa!! 
-//   This might be cool or might be terrible. It is very JavaScript Engine!
 //
 // - Implement free as a non-noop for stack frames on the bump allocator.
