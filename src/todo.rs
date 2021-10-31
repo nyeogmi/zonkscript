bd4246000f72cc2d5e2e5d2c71b9b2d8937ae44d
Nyeogmi <economicsbat@gmail.com>
2021-10-30 20:32:45 -0700

    Update TODOs more

--- src/todo.rs
+++ src/todo.rs
@@ -1,6 +1,8 @@
 // TODO: Things below.
 // - Don't use Moogle (Use something ad hoc instead.)
 // - Get rid of hacks for 1-indexing.
+// - Actually check for whether functions and structs haven't been sealed -- error if so.
+// - Fail gracefully; don't panic.
 // - std_primitives.rs and linker.rs are _real ugly
 // - Variant should be Copy.
 // - Arguments for Call instruction.
